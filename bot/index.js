0c8202922085383d82ed8e73d8741556196f15b6
Pierre PATAKI <ppataki@sdv.fr>
2019-09-25 18:03:03 +0200

    init(project)

---
+++ bot/index.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./credentials/auth.json');
+const adminRoleName = 'Fondateurs';
+
+
+
+let voteInProgress = false;
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    client.user.setPresence({
+        game: {
+            name: '!vote'
+        }
+    })
+});
+
+client.on('message', function(message) {
+    let firstVote = ":one: Normale (Zone Rapide/Mortelle)\n:two: Course de voiture/moto\n:three: War mode'";
+
+    let emojiOne   = message.guild.emojis.find(emoji => emoji.name === "one");
+    let emojiTwo   = message.guild.emojis.find(emoji => emoji.name === "two");
+    let emojiThree = message.guild.emojis.find(emoji => emoji.name === "three");
+
+    let voteChannel = client.channels.find(channel => channel.name === "votes");
+
+    // react only on !vote messages
+    if (message == '!vote' && !voteInProgress) {
+        // react only to admins
+        if (!message.member.roles.find(r => r.name === adminRoleName)) {
+            message.reply(`You are not authorized to start a new custom vote.`);
+        } else {
+            console.log(emojiOne);
+
+            voteChannel
+                .send(firstVote)
+                .then(postedMessage => {
+                    postedMessage
+                        .react(emojiOne)
+                        .react(emojiTwo)
+                        .react(emojiThree);
+                });
+        }
+    }
+});
+
+client.login(auth.token);
